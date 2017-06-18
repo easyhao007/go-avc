db2b6c3090e5b0e54fe4cc1c6fa0938d3c73f356
easyhao007 <easyhao007@163.com>
2017-06-18 16:19:37 +0800

    Initial commit

---
+++ avc/avcHead.go
@@ -0,0 +1,13 @@
+package avc
+
+import(
+	"fmt"
+)
+
+func Introduce(){
+	fmt.Println("解析AVC的头")
+}
+
+type avcHead struct{
+	
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+"fmt"
+)
+
+func main(){
+	fmt.Println("h264结构分析学习:")
+}
+
